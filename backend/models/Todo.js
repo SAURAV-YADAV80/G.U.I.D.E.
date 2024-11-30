2d101d3d5a2780f55fd6e609f8a39e1a1aa41c9c
SAURAV-YADAV80 <yadavjiji8075@gmail.com>
2024-11-30 14:32:01 +0530

    updated

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const todoSchema = new mongoose.Schema({
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
+  email: {type:String, required: true}
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
--- todo-frontend/src/pages/Reff.jsx
+++
@@ -1,9 +0,0 @@
-import SocialPlatforms from "../components/SocialPlateforms";
-import { socialPlatformsData } from "../data/socialPlateformsData";
-
-
-const Reff = () => {
-  return <SocialPlatforms platforms={socialPlatformsData} />;
-};
-
-export default Reff;
